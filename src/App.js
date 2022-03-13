35b3dd9e0e2adcb9f53921ee31320fe377578499
DragonWakefield <dwakefie@uwo.ca>
2022-03-13 14:43:47 -0400

    Undid changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 import SeagullInfo from './components/seagullinfo';
 
 
@@ -6,6 +8,24 @@ function App() {
   return (
     <div className="App">
       <SeagullInfo />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button variant="success" className="p-2">Primary</Button>
+        
+      </header>
+      
+      
     </div>
   );
 }
