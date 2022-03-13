ae75b64f72d59447e3d2bbbd376ff918f91c450b
Caio C. Coelho <ccoelho6@uwo.ca>
2022-03-13 14:45:39 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
-import SeagullInfo from './components/seagullinfo';
-
 
 function App() {
   return (
     <div className="App">
-      <SeagullInfo />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -22,10 +19,7 @@ function App() {
           Learn React
         </a>
         <Button variant="success" className="p-2">Primary</Button>
-        
       </header>
-      
-      
     </div>
   );
 }
