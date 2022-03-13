c507cacc92971fab36def5480299d86100192782
Scott Parkhill <scottparkhill@culturefoundry.ca>
2022-03-13 12:37:47 -0400

    Initializes base functionality of settings component.

---
+++ src/components/settings.js
@@ -0,0 +1,39 @@
+import component from react;
+
+class Settings extends React.component {
+
+    
+    constructor(props) {
+        super(props);
+
+        let default_settings = {
+            dark_mode: false,
+            week_starts_on: "Monday",
+            default_task_folder: null,
+            seagull_icon: "default.jpg",
+            tasks_for_level_up: 5,
+            seagull_recording: "default.mp3",
+            seagull_scream_disabled: false                
+        };
+
+        this.state = default_settings;
+
+        // If no settings file exists, create it with default parameters.
+        if (localStorage.getItem("seagull_do_settings") === null) {
+            localStorage.setItem("seagull_do_settings", JSON.stringify(default_settings));
+        } else {
+            this.state = localStorage.getItem("seagull_do_settings");
+        }
+        
+    }
+
+    toggleDarkMode = () => this.state[dark_mode] != this.state[dark_mode];
+    setWeekStartsOn = (day) => this.state[week_starts_on] = day;
+    setDefaultTaskFolder = (folder_name) => this.state[default_task_folder] = folder_name;
+    setSeagullIcon = (icon) => this.state[seagull_icon] = icon;
+    setTaskForLevelUp = (num) => this.state[tasks_for_level_up] = num;
+    setSeagullRecording = (recording) => this.state[seagull_recording] = recording;
+    toggleSeagullScream = () => this.state[seagull_scream_disabled] != this.state[seagull_scream_disabled];
+
+    render() { <h1>Settings</h1>}
+}
